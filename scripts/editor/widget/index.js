9686bf5b41416e90cb4d8a68d590a1342d355963
winter <csf178@gmail.com>
2012-08-21 19:13:46 +0800

    power with wind.js

--- scripts/editor/widget/index.js
+++ scripts/editor/widget/index.js
@@ -1,13 +1,15 @@
 define(['editor/widget/slide',
     'editor/widget/blank',
     'editor/widget/text',
-    'editor/widget/img'
-], function (Slide, Blank, Text, Img) {
+    'editor/widget/img',
+    'editor/widget/demo'
+], function (Slide, Blank, Text, Img, Demo) {
     return {
         'slide': Slide,
         '': Blank,
         'text': Text,
         'title': Text,
-        'img': Img
+        'img': Img,
+        'demo': Demo
     };
 });
