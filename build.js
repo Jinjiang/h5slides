f66f38bb3900322bcde9b1bce773507f1cf84cb8
Jinjiang <zhaojinjiang@yahoo.com.cn>
2012-11-07 00:36:28 +0800

    added build tools

---
+++ build.js
@@ -0,0 +1,70 @@
+({
+    appDir: "./",
+    baseUrl: "./scripts",
+    dir: "./build",
+
+    //As of RequireJS 2.0.2, the dir above will be deleted before the
+    //build starts again. If you have a big build and are not doing
+    //source transforms with onBuildRead/onBuildWrite, then you can
+    //set keepBuildDir to true to keep the previous dir. This allows for
+    //faster rebuilds, but it could lead to unexpected errors if the
+    //built code is transformed in some way.
+    keepBuildDir: true,
+
+    //How to optimize all the JS files in the build output directory.
+    //Right now only the following values
+    //are supported:
+    //- "uglify": (default) uses UglifyJS to minify the code.
+    //- "closure": uses Google's Closure Compiler in simple optimization
+    //mode to minify the code. Only available if running the optimizer using
+    //Java.
+    //- "closure.keepLines": Same as closure option, but keeps line returns
+    //in the minified files.
+    //- "none": no minification will be done.
+    optimize: "none",
+
+    //Allow CSS optimizations. Allowed values:
+    //- "standard": @import inlining, comment removal and line returns.
+    //Removing line returns may have problems in IE, depending on the type
+    //of CSS.
+    //- "standard.keepLines": like "standard" but keeps line returns.
+    //- "none": skip CSS optimizations.
+    //- "standard.keepComments": keeps the file comments, but removes line
+    //returns.  (r.js 1.0.8+)
+    //- "standard.keepComments.keepLines": keeps the file comments and line
+    //returns. (r.js 1.0.8+)
+    optimizeCss: "standard.keepLines",
+
+    //cssIn is typically used as a command line option. It can be used
+    //along with out to optimize a single CSS file.
+    // cssIn: "path/to/main.css",
+    // out: "path/to/css-optimized.css",
+
+    //If set to true, any files that were combined into a build layer will be
+    //removed from the output folder.
+    removeCombined: true,
+
+    //List the modules that will be optimized. All their immediate and deep
+    //dependencies will be included in the module's file when the build is
+    //done. If that module or any of its dependencies includes i18n bundles,
+    //only the root bundles will be included unless the locale: section is set above.
+    modules: [
+        //Just specifying a module name means that module will be converted into
+        //a built file that contains all of its dependencies. If that module or any
+        //of its dependencies includes i18n bundles, they may not be included in the
+        //built file unless the locale: section is set above.
+        {
+            name: "main",
+            exclude: ['lib/zepto']
+        },
+
+        //This module entry combines all the dependencies of foo/bar/bop and foo/bar/bee
+        //and any of their dependencies into one file.
+        {
+            name: "main-player",
+            exclude: ['lib/zepto']
+        }
+    ],
+
+    fileExclusionRegExp: /^(test|r\.js|build|\.|[A-Z])/,
+})
