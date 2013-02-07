e2260c59408d3a36f39ad65eed4962fd6c67cb5f
勾三股四 <zhaojinjiang@yahoo.com.cn>
2013-02-07 19:06:43 +0800

    basic mvvm test with template list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 require(
-    ['ui/title', 'ui/menu', 'ui/template', 'ui/pagenav', 'ui/stage'],
-    function (title, menu, template, pagenav, stage) {
+    [],
+    function () {
     }
 );
--- js/ui/template.js
+++ js/ui/template.js
@@ -1,5 +1,1 @@
-define(['data/index'], function (data) {
-    var vm = data.vm;
-    vm.templateList = ko.observableArray(data.template);
-    vm.currentTemplate = ko.observable('title');
-});
+define({});
